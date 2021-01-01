2370cd4a0fb6984b8106c705b62a7a687b92c5be
clearlynow <59779189+clearlynow@users.noreply.github.com>
2021-01-01 16:38:56 -0700

    Initial Commit

---
+++ BrainTraining/BrainTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingApp.swift
+//  BrainTraining
+//
+//  Created by Alison Gorman on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
